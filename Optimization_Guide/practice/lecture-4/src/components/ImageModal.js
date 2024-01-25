7eb18c7202926d92b3619ec9409561e198dc1829
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2024-01-25 15:01:47 +0900

    [Optimization_Guide] : 04_이미지 갤러리 최적화

--- Optimization_Guide/practice/lecture-4/src/components/ImageModal.js
+++ Optimization_Guide/practice/lecture-4/src/components/ImageModal.js
@@ -7,23 +7,20 @@ import { getAverageColorOfImage } from '../utils/getAverageColorOfImage';
 
 function ImageModal({ modalVisible, src, alt, bgColor }) {
   const dispatch = useDispatch();
-  const onLoadImage = e => {
-    const averageColor = getAverageColorOfImage(e.target);
-    dispatch(setBgColor(averageColor));
-  };
+  // const onLoadImage = e => {
+  // const averageColor = getAverageColorOfImage(e.target);
+  // dispatch(setBgColor(averageColor));
+  // };
 
   const closeModal = () => {
     dispatch(hideModal());
   };
 
   return (
-    <Modal
-      modalVisible={modalVisible}
-      closeModal={closeModal}
-      bgColor={bgColor}
-    >
+    <Modal modalVisible={modalVisible} closeModal={closeModal} bgColor={bgColor}>
       <ImageWrap>
-        <FullImage crossOrigin="*" src={src} alt={alt} onLoad={onLoadImage} />
+        {/* <FullImage crossOrigin="*" src={src} alt={alt} onLoad={onLoadImage} /> */}
+        <FullImage crossOrigin="*" src={src} alt={alt} />
       </ImageWrap>
     </Modal>
   );
--- Optimization_Guide/practice/lecture-4/src/components/PhotoItem.js
+++ Optimization_Guide/practice/lecture-4/src/components/PhotoItem.js
@@ -1,27 +1,49 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
-import { showModal } from '../redux/imageModal';
+import { setBgColor, showModal } from '../redux/imageModal';
+import LazyLoad from 'react-lazyload';
+import { getAverageColorOfImage } from '../utils/getAverageColorOfImage';
 
 function PhotoItem({ photo: { urls, alt } }) {
   const dispatch = useDispatch();
 
-  const openModal = () => {
+  const openModal = e => {
     dispatch(showModal({ src: urls.full, alt }));
+
+    // 섬네일 이미지로 배경색 계산 후, 리더긋에 저장
+    const averageColor = getAverageColorOfImage(e.target);
+    dispatch(setBgColor(averageColor));
   };
 
   return (
     <ImageWrap>
-      <Image src={urls.small + '&t=' + new Date().getTime()} alt={alt} onClick={openModal} />
+      <LazyLoad offset={1000}>
+        <Image
+          src={urls.small + '&t=' + new Date().getTime()}
+          alt={alt}
+          onClick={openModal}
+          crossOrigin="*"
+        />
+      </LazyLoad>
     </ImageWrap>
   );
 }
 
-const ImageWrap = styled.div``;
+const ImageWrap = styled.div`
+  /*너비는 상위 컴포넌트에서 정의*/
+  width: 100%;
+  padding-bottom: 56.25%;
+  position: relative;
+`;
 
 const Image = styled.img`
   cursor: pointer;
   width: 100%;
+  position: absolute;
+  height: 100%;
+  top: 0;
+  left: 0;
 `;
 
 export default PhotoItem;
